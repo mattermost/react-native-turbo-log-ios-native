75bbc7d9b0c2358810dc294216dd72a03bd35119
Elias Nahum <nahumhbl@gmail.com>
2025-06-12 16:48:42 +0800

    Create TurboLogIOSNative.h

---
+++ Sources/TurboLogSwift/TurboLogIOSNative.h
@@ -0,0 +1,10 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for TurboLogIOSNative.
+FOUNDATION_EXPORT double TurboLogIOSNativeVersionNumber;
+
+//! Project version string for TurboLogIOSNative.
+FOUNDATION_EXPORT const unsigned char TurboLogIOSNativeVersionString[];
+
+// Import all public headers
+#import "TurboLog.h"
